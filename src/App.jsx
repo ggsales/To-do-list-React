ae02c1e52a20c9f80afb18e8ffc975a80d438ed9
ggsales <giovanisales14@gmail.com>
2021-10-08 12:22:24 -0300

    create button and input component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 import Tasks from './components/Tasks'
 import "./App.css"
+import AddTask from "./components/AddTask";
 
 
 const App = () => {
@@ -21,6 +22,7 @@ const App = () => {
     return( 
       <>
         <div className= "container">
+          <AddTask />
           <Tasks tarefas={tarefas}/>
         </div>;
       </>
